f1f4a5c29cb763bbc08145715ea540be81728599
derekc4859 <derekc4859@hstat.org>
2016-12-19 22:24:30 +0000

    Finish homework!

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,27 @@
 # Your code goes here!
+class Dog
+    def Initialize
+         name
+         bark
+    end
+    
+    def name=(dog_name)
+        @name = dog_name
+    end
+    
+    def name
+        @name
+    end
+    def bark
+        puts "woof!"
+    end
+end
+
+fido = Dog.new
+fido.name = "Fido"
+
+fido.name
+
+fido.bark
+
+fido.name
