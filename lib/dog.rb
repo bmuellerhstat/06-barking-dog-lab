24d14b8f16228dcb8198b07fbf8172e859092d81
Jenny Feng <jennyfeng.ga.ling1999@gmail.com>
2016-12-20 16:43:09 +0000

    add comments for self

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,15 +1,15 @@
 # Your code goes here!
 class Dog # overall factory/ class called "Dog"
     def initialize # instance or similarly a method to define the dog name
-        @name = name
+        @name = name # adds name as variable
     end
     def bark
         puts "woof!"
     end
     def name
-        @name
+        @name #adds name as string
     end
     def name=(name)
-        @name = name
+        @name = name #aads name as string
     end
 end
