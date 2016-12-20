838c392e0227e7abb32466e14b7dc503c0fa1ca2
jeremye3180 <Jeremye3180@hstat.org>
2016-12-20 11:40:33 +0000

    finish hw

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,20 @@
 # Your code goes here!
+class Dog
+    def initalize
+        name
+        bark
+    end
+    @name
+    
+    def name
+    @name
+    end
+
+    def name=(dog)
+    @name = dog
+    end
+
+    def bark
+    puts "woof!"
+    end
+end
