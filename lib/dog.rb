8e56671cf854fb5d659082f11dacb92614327dde
Jenny Feng <jennyfeng.ga.ling1999@gmail.com>
2016-12-20 16:18:56 +0000

    complete 1st and 3rd failures

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,9 @@
 # Your code goes here!
+class Dog # overall factory/ class called "Dog"
+    def dog_name(name) # instance or similarly a method to define the dog name
+        @name
+    end
+    def bark
+        puts "woof!"
+    end
+end
