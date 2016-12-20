90e2b4a3204273c8657a65bb87a639329f7c1405
joannef3188 <joannef3188@hstat.org>
2016-12-20 18:25:36 +0000

    Finish hw

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,13 @@
-# Your code goes here!
+class Dog
+    def bark
+        puts "woof!"
+    end
+    
+    def name
+        @name
+    end
+  
+    def name=(full_name)
+        @name = full_name
+    end
+end
