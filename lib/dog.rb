f23e91615655ad83a624d8cdf05db555360fa7dd
rawfurrahman1423 <rawfurr1423@hstat.org>
2016-12-20 18:18:50 +0000

    finished and IDK WHAT IS WRONG

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,28 @@
 # Your code goes here!
+class Dog
+    def Initialize
+        name
+        bark
+    end
+    
+    def name=(pet_name)
+        @name = pet_name
+    end
+    
+    def name 
+        @name
+    end
+    def bark
+        puts "woof!"
+    end
+end
+
+fido = Dog.new
+fido.name = "Fido"
+
+fido.name
+# => "Fido"
+
+fido.bark
+woof!
+# => nil
