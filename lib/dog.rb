d149ca25222d0d86b5525259d0327b3e0ef05d58
xinyanc3694 <xinyanc3694@hstat.org>
2017-12-14 02:18:28 +0000

    done hw

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,12 @@
 # Your code goes here!
+class Dog
+    def  name
+        @dog_name 
+    end
+    def name= (name)
+        @dog_name = name 
+    end
+    def bark
+        puts "woof!" 
+    end
+end 
