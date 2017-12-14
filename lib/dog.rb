6771cba3a544dfea794cc19cd1cbc833eff36da1
biannak6288 <biannak6288@hstat.org>
2017-12-14 00:58:35 +0000

    finish lab

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,12 @@
 # Your code goes here!
+class Dog
+    def name= name
+       @dog_name = name #set name to inst vari/give dog a name
+    end
+    def name        
+        @dog_name #return inst vari/indiv dog name
+    end
+    def bark
+        puts "woof!" #barks
+    end
+ end 
