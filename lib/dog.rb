7ec7d074fbb3f6792bdd75e2d9669ae21c007d2f
Kellyx1636 <kellyx1636@hstat.org>
2017-12-14 20:39:27 +0000

    Solved and completed Dog class.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,19 +1,22 @@
 # Your code goes here!
 class Dog 
-    def initialize(name)
-        bark 
-        @name = name
+    def initialize() #Parameter not needed? Does not work with 'name' as parameter 
+      bark 
+      #Program works even without line 6 because of line 18? 
+      # @name = name 
     end
     
     def bark 
-        puts "woof!"
+      puts "woof!"
     end
     
-    def name #setter/writer method 
-        @name
+    #getter/reader method
+    def name  
+      @name
     end 
     
-    def name=(name)
-        @name = name 
+    #setter/writer method
+    def name= (new_name)  #Instead of 'name', it is 'new_name'. Just in case a new name replaces the other one? 
+      @name = new_name  
     end
-end
+end 
