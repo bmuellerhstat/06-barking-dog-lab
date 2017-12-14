3d19d2032acb9daacd93f33142f2f5c038ce8a98
Kellyx1636 <kellyx1636@hstat.org>
2017-12-14 13:52:23 +0000

    Dog class unsuccessfully attempted.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,19 @@
 # Your code goes here!
+class Dog 
+    def initialize(name)
+        bark 
+        @name = name
+    end
+    
+    def bark 
+        puts "woof!"
+    end
+    
+    def name #setter/writer method 
+        @name
+    end 
+    
+    def name=(name)
+        @name = name 
+    end
+end
