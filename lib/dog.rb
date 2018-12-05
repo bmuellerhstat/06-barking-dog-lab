22ef77c283e4312ab445f7860dc0741e7ed50e67
Eva Noertoft <evan2304@hstat.org>
2018-12-05 21:46:06 +0000

    add comments

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,13 +1,14 @@
 # Your code goes here!
 class Dog
+    #setter method that gives dog name
     def name=(name)
         @name = name
     end
-    
+    #getter method that returns an individual dogs name
     def name 
         @name
     end
-    
+    #bark method
     def bark
         puts "woof!"
     end
