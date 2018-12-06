b943143550e555248fcf1bea675678319dc28964
Ahmad Rabah <ahmadr9279@hstat.org>
2018-12-06 03:09:45 +0000

    fix all errors

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,11 @@
-# Your code goes here!
+class Dog
+    def bark
+        puts "woof!"
+    end
+    def name= (dogname)#setter method which updates value
+        @dogname = dogname
+    end
+    def name#getter method which retrieves value
+        @dogname
+    end
+end
