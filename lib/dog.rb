8fca9898f4d971792b77b26a4bcab19e8db08009
Danny Wu <dannyw3292@hstat.org>
2018-12-06 03:33:03 +0000

    complete assignment

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,18 @@
 # Your code goes here!
+class Dog
+    def init(name)
+        bark
+    end
+    
+    def bark
+        puts "woof!"
+    end
+    
+    def name
+       @name 
+    end
+    
+    def name=(name)
+        @name = name
+    end
+end
