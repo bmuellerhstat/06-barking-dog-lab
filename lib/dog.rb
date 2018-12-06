bae14bba4339027cb989bad9a6a38d3c2e1cbec2
Shi Wei Zheng <shiz3150@hstat.org>
2018-12-06 00:20:13 +0000

    resolve errors

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,16 @@
 # Your code goes here!
+class Dog
+    
+    def name=(name)
+        
+    end
+    
+    def name
+        @fido = Dog.new
+        @fido.name = "Fido"
+    end
+    
+    def bark
+        puts "woof!"
+    end
+end
